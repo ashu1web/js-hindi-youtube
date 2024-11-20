cea0d84d6ec14f18b90484e31553f009ea0e0ae0
ashu1web <113449680+ashu1web@users.noreply.github.com>
2024-11-20 13:01:05 +0530

    Update Promises.js

--- 08_Advance/Promises.js
+++ 08_Advance/Promises.js
@@ -96,6 +96,3 @@ fetch('https://api.github.com/users/hiteshchoudhary') //fetch is a promise
    console.log(data);
 })
 .catch((error) => console.log(error))
-
-// promise.all
-// yes this is also available, kuch reading aap b kro. 
